a239ac5f1e65eec60ca005b0da137b4cef51c491
keeeper85 <122378010+keeeper85@users.noreply.github.com>
2024-04-19 14:02:38 +0800

    Initial commit

---
+++ src/test/java/com/keeeper/learningmicroservices/LearningMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keeeper.learningmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
